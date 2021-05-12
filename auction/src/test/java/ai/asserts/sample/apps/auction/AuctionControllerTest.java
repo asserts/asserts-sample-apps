cc4891f4ccb9fee0a0e480d935b22b368069a3e5
jradhakrishnan <radhakrishnan.jankiraman@gmail.com>
2021-05-12 16:16:35 +0530

    Added CPU and Memory Usage APIs Introduced role as server or test client

---
+++ auction/src/test/java/ai/asserts/sample/apps/auction/AuctionControllerTest.java
@@ -0,0 +1,13 @@
+/*
+ *  Copyright © 2020.
+ *  Asserts, Inc. - All Rights Reserved
+ */
+package ai.asserts.sample.apps.auction;
+
+import org.junit.jupiter.api.Test;
+
+public class AuctionControllerTest {
+    @Test
+    void compress() {
+    }
+}
